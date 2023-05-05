fb3960e7e1f59e267855711acf2a5eab8c031c0f
Izabela <104584812+izabela-jelovac@users.noreply.github.com>
2023-05-06 01:09:43 +0200

    add menu component

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Card from "./components/card.component";
+import Menu from "./components/menu.component";
 
 function App() {
   return (
     <div className="App">
+      <Menu />
       <Card title={"hello world"} description={"hi"} />
       <Card title={"goodbye world"} description={"bye"} />
     </div>
---
+++ personal-website/src/components/menu.component.js
@@ -0,0 +1,8 @@
+function Menu() {
+  return (
+    <div>
+      <p>MENU</p>
+    </div>
+  );
+}
+export default Menu;
