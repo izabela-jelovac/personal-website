f5af46353971c2ee7810c7c06687e52b32d28bab
Izabela <104584812+izabela-jelovac@users.noreply.github.com>
2023-05-08 23:17:41 +0200

    fix menu item style

--- personal-website/src/Education.js
+++ personal-website/src/Education.js
@@ -3,17 +3,25 @@ function Education() {
     <div>
       <h1>Education</h1>
       <div className="menu-item">
-        <h3>Front end developer</h3>I have completed Front end developer course
-        at Otvoreno učilište Algebra. I have experience using Visual Studio Code
-        and Git, and have used HTML, CSS, JavaScript and React on my projects. I
-        am familiar with jQuery, Redux, Bootstrap and Node.js. I am looking
-        forward to learning and gaining new experiences.
-        <h3>Internet marketing specialist</h3>I have completed Internet
-        marketing specialist course at Otvoreno učilište. I have used Google
-        Ads, Google Analytics, Google Search Console and Google tag manager. My
-        goal is to create high quality campaigns making better page ranking
-        using SEO, SERP and page rank creating web site.
+        <h3>Front end developer</h3>
+        <div className="education">
+          I have completed Front end developer course at Otvoreno učilište
+          Algebra. I have experience using Visual Studio Code and Git, and have
+          used HTML, CSS, JavaScript and React on my projects. I am familiar
+          with jQuery, Redux, Bootstrap and Node.js. I am looking forward to
+          learning and gaining new experiences.
+        </div>
+
+        <h3>Internet marketing specialist</h3>
+        <div className="education">
+          I have completed Internet marketing specialist course at Otvoreno
+          učilište. I have used Google Ads, Google Analytics, Google Search
+          Console and Google tag manager. My goal is to create high quality
+          campaigns making better page ranking using SEO, SERP and page rank
+          creating web site.
+        </div>
         <h3>Bachelor's degree (baccalaureus) safety and protection engineer</h3>
+        <div className="education"></div>
       </div>
     </div>
   );
--- personal-website/src/MyProjects.js
+++ personal-website/src/MyProjects.js
@@ -4,7 +4,7 @@ function MyProjects() {
       <h1>My projects</h1>
       <div className="menu-item">
         I am still learning how to code, so I am creating simple projects. You
-        can check some of my code on my 
+        can check some of my code on my
         <a href="https://github.com/izabela-jelovac?tab=repositories">
           GitHub account
         </a>
